c255fe01266c482321705fc81153a92a85b97aa0
Rostand TCHAMEN <tchamenrostand@yahoo.com>
2025-11-25 12:19:03 +0100

    Mise à jour code

---
+++ src/main/java/com/sinbad/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.sinbad;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HelloServlet", urlPatterns = {"/api/hello"})
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = resp.getWriter();
+        out.write("{\"message\": \"Hello from Sinbad Java API via Tomcat !\"}");
+        out.flush();
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Sinbad Java API</title>
+</head>
+<body>
+<h1>Sinbad Java API</h1>
+<p>Appli déployée sur Tomcat et servie via nginx.</p>
+<p>Test API : <a href="api/hello">/api/hello</a></p>
+</body>
+</html>
